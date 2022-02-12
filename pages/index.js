f581aa483c7442184b3a88e3ab678df91160ebbb
David Bender <bender2242@gmail.com>
2022-02-12 19:22:51 +0000

    First iteration of app from the course, focusing on routing and nested structure

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Link from 'next/link';
 function HomePage() {
 	return (
 		<div>
-			<h1>Portfolio App Home Page</h1>
+			<h1>Portfolio App</h1>
+			<h3>Useful for seeing routing and nested page structures.</h3>
 			<ul>
 				<li>
 					<Link href='/portfolio'>Portfolio</Link>
