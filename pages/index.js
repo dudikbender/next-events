b5db233ab63c8b4429bba48622d795a72f415134
David Bender <bender2242@gmail.com>
2022-03-27 21:00:23 +0100

    added comments feed

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
 import classes from '../styles/Home.module.css';
+import NewsletterRegistration from '../components/input/newsletter-registration'
 
 function HomePage(props) {
 	return (
@@ -13,6 +14,7 @@ function HomePage(props) {
 				name='description' 
 				content='Search here for great events'/>
 			</Head>
+			<NewsletterRegistration />
 			<h1 className={classes.title}>Featured Events</h1>
 			<EventList items={props.events} />
 		</div>
